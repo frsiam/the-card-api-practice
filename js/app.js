acd195496e1867d020ae68852a0307f53c27eaea
frsiam <siam.frs@gmail.com>
2022-02-28 00:41:24 +0600

    done the project

--- js/app.js
+++ js/app.js
@@ -7,23 +7,28 @@ function error(errorMessage){
 }
 function success(){
     Swal.fire({
-        position: 'top-center',
+        position: 'center',
         icon: 'success',
         title: 'Your work has been done',
         showConfirmButton: false,
         timer: 1500
       })
 }
-
+// catch the id of div where we show the card details 
+const mainDiv = document.getElementById('show-card')
+// check the input search and handle the error and get the fetch of url 
 const searchCards = () => {
     const searchinput = document.getElementById('search-card')
-    const searchValue = searchinput.value
+    const searchValue = parseInt(searchinput.value)
+
     if(isNaN(searchValue) || searchValue == "" ){
+        mainDiv.textContent = ''
         error('Input must be a positive number')
         searchinput.value = ''
     }
     else if(searchValue <= 0 || searchValue > 52){
-        error('Must be greater than zero and less than fifty two');
+        mainDiv.textContent = ''
+        error('Must be greater than 0 and less than 52');
         searchinput.value = ''
     }
     else{
@@ -34,6 +39,26 @@ const searchCards = () => {
         searchinput.value = '' 
     }
 }
+// show the card into ui 
 const cardsDisplay = inputCards => {
     console.log(inputCards)
+    mainDiv.textContent = ''
+    for(const card of inputCards.cards){
+        console.log(card)
+        const div = document.createElement('div')
+        div.className = 'col'
+        div.innerHTML = `
+            <div class="card h-100 border-danger">
+                <img src="${card.images.png}" class="card-img-top p-2 img-fluid bg-primary" alt="...">
+                <div class="card-body bg-dark">
+                <h5 class="card-title text-white">${card.suit}</h5>
+                <ul class='border border-2 border-info'>
+                    <li class='text-warning'><span class='text-primary fw-bold'>Code : </span>${card.code}</li>
+                    <li class='text-warning'><span class='text-primary fw-bold'>Value : </span>${card.value}</li>
+                </ul>
+            </div>
+        `
+        mainDiv.appendChild(div)
+    }
+     
 }
